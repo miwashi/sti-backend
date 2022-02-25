e5cae450c648df3ffa9854889ed9abbaa00f222b
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-02-25 12:03:17 +0100

    Working auth

--- app.js
+++ app.js
@@ -1,16 +1,64 @@
 const cors = require("cors")
 const express = require("express")
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(function (req, res, next) {
+   //res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
+   next();
+});
+
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+app.use(bodyParser.json());
 
-app.get("/", (req ,res)=>{
+app.get("/", (request ,response)=>{
    headers={"http_status":200, "cache-control":  "no-cache"}
    body={"status": "available"}
+   response.status(200).send(body)
+})
+
+app.get("/registerscore", (req ,res)=>{
+   headers={"http_status":200, "cache-control":  "no-cache"}
+   body={"name": req.query.name, "score":req.query.score}
+   
+   //Fattas kod för att spara i en dictionary
+   res.status(200).send(body)
+})
+
+app.post('/echo', (req, res) => {
+   console.log(JSON.stringify(req.body))
+   res.status(200).send(req.body)
+});
+
+app.post('/auth', (req, res) => {
+   let user = req.body.user;
+   let password = req.body.password;
+   console.log(req.body)
+   console.log(`User ${user}`)
+   console.log(`Password ${password}`)
+   if(true){
+      console.log("Authorized")
+      const token = jwt.sign({
+            data: 'foobar'
+      }, 'your-secret-key-here', { expiresIn: 60 * 60 }); 
+      console.log(token)
+      res.status(200).send(token)
+  }else{
+      console.log("Not authorized")
+      res.status(200).send({"STATUS":"FAILURE"})
+   }
+});
+
+app.get("/higscore", (req ,res)=>{
+   headers={"http_status":200, "cache-control":  "no-cache"}
+   body=[{"name": "nisse", "score":"1000"}, {"name": "olle", "score":"1001"}, {"name": "pelle", "score":"3000"}]
+   //Istället för att hårdkoda, läs ut från dictionary
+   
    res.status(200).send(body)
 })
 
@@ -22,24 +70,9 @@ app.get("/football", (req ,res)=>{
             "name": "AIK",
             "points": 9,
             "logo":"https://www.allsvenskan.se/lagen/aik/_/image/0c02711d-d44e-4124-a555-0df3d8264551:0fb201f22e823629dada3ee33327a5b70963120b/width-110/AIK.svg"
-      },
-      {
-            "name": "DIF",
-            "points": 6,
-            "logo":"https://www.allsvenskan.se/lagen/aik/_/image/939ceff1-1923-48b0-af9a-997dfe4fba51:126ef93a454f04822c9f8575831a0871abd06398/width-110/Djurg%C3%A5rden.svg"
-      },
-      {
-            "name": "BK Häcken",
-            "points": 5,
-            "logo":"https://www.allsvenskan.se/lagen/bk-hacken/_/image/29528574-2d7b-43e2-9b91-89f3bfa74bdc:f981f4253c7e9711fe2c9449765cda15327af8aa/width-110/BK_H%C3%A4cken_logo.svg"
-      },
-      {
-            "name": "Degerfors IF",
-            "points": 3,
-            "logo":"https://www.allsvenskan.se/lagen/bk-hacken/_/image/e58c3846-9f1a-4bde-9ccb-99df65cc5360:c14b6bb4bc1d5954b698254ffed2e5a4593a1b6b/width-110/Degerfors%20IF.svg"
       }
    ]
-   
+
    res.set('Content-Type', 'application/json');
    res.status(200).send(body)
 })
